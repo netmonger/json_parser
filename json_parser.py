092caa7d1498d19f02043f6c870bc3a734cc6987
Rodney <netmonger@users.noreply.github.com>
2015-07-10 17:21:47 -0500

    Added stuff

--- json_parser.py
+++ json_parser.py
@@ -1,6 +1,22 @@
 import json
-from pprint import pprint
+import pprint
 
 with open('ning-discussions.json') as data_file:    
 	    data = json.load(data_file)
-	    pprint(data)
+	    # pprint(data)
+
+entry1 = data[0]
+
+topicID = entry1["id"]
+contributorName = entry1["contributorName"]
+title = entry1["title"]
+description = entry1["description"]
+createdDate = entry1["createdDate"]
+updatedDate = entry1["updatedDate"]
+comments = entry1["comments"]
+
+print "Title: %s" % title
+print "Author: %s" % contributorName
+print "Date: %s" % createdDate
+print 
+print description
